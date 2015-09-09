f32e8ce9747d43088c798c0a8df5183edb45b3ee
Jesse Shawl <jesse@jshawl.com>
2015-09-09 09:12:04 -0400

    rm the things

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source "https://rubygems.org"
-
-gem 'rake'
-gem 'minitest'
-gem 'awesome_print'
-gem 'pry-byebug'
--- installfest.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# This file lives in github pages so the reference url is "clean".  If we store this in the master branch, the url would be to "https://raw.githubusercontent..."
-
-# download the shared Rakefile
-curl --location https://raw.githubusercontent.com/ga-dc/installfest/master/Rakefile > Rakefile
-# Download the config for THIS class
-curl --location https://raw.githubusercontent.com/ga-dc/installfest/master/installfest_dc_wdi.yml > installfest.yml
