687f05677447ae044e759a98198000601d23f82f
superbuggy <jpqreichard@gmail.com>
2017-08-01 09:39:18 -0400

    fix curl urls

--- installfest.sh
+++ installfest.sh
@@ -3,6 +3,6 @@
 # This file lives in github pages so the reference url is "clean".  If we store this in the master branch, the url would be to "https://raw.githubusercontent..."
 
 # download the shared Rakefile
-curl --location https://git.generalassemb.ly/raw/ga-dc/installfest/master/Rakefile > Rakefile
+curl --location https://git.generalassemb.ly/raw/DC-WDI/Installfest/master/Rakefile > Rakefile
 # Download the config for THIS class
-curl --location https://git.generalassemb.ly/raw/ga-dc/installfest/master/installfest_dc_wdi.yml > installfest.yml
+curl --location https://git.generalassemb.ly/raw/DC-WDI/installfest/master/installfest_dc_wdi.yml > installfest.yml
